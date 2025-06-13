7aff024ef047e2111f6225d040345395dacede54
Gabriel <gabrielholanda18@hotmail.com>
2025-06-13 01:57:01 -0300

    Função adicionada de medição de poder do robo

--- Duelo.py
+++ Duelo.py
@@ -4,3 +4,8 @@ def duel(robo1, robo2, duel_lock):
         loser = robo2 if winner == robo1 else robo1
         print(f"{winner.name} venceu o duelo contra {loser.name}!")
         return winner
+
+def _calcular_poder(forca, energia):
+    """Calcula o poder de ataque do robô conforme o PDF."""
+    return 2 * forca + energia # Poder=2F+E 
+
